dcf22d78a1ae60da6293f1f173d1687c4c6d0f83
CruzWin <javier1996.ec@gmail.com>
2023-07-29 11:48:59 +0200

    init Proy

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    TouchableOpacityBase,
+    Platform, // Para plataformas
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>POMODORO</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        margin: Constants.statusBarHeight,
+        // flex: 1,
+        // backgroundColor: '#fff',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
 });
